8f2d352daa7dc7f1fc305ccf2ec9fa723c554f01
张晋 <zhangjin@corp.netease.com>
2018-03-22 14:47:08 +0800

    Init

---
+++ misc/gen/userlog_gen.py
@@ -0,0 +1,38 @@
+﻿#--umport random
+import sys,time,random
+import urllib,urllib2
+
+user_gif_url = "http://localhost:8000/user.gif"
+
+
+def rand(values=[]):
+    rnd = random.randint(0, len(values) - 1)
+    return values[rnd]
+
+def rand_arr(arr=[]):
+    rnd = random.randint(1, len(arr) - 1)
+    return arr[0:rnd]
+
+def tracker(ptc=None, uid=None):
+    params = {
+        "_ptc": ptc,
+        "_puid": uid,
+        "_pua_gender": rand(['m', 'f']),
+        "_pua_isvip": rand(['t', 'f']),
+        "_pua_viplevel": rand(['1', '2', '3', '4', '5']),
+        "_pua_platform": ",".join(rand_arr(['aos', 'ios', 'web'])),
+        "_puan_age": random.randint(1, 80)
+    }
+
+    url = user_gif_url + "?" + urllib.urlencode(params)
+    print url
+    urllib2.urlopen(url)
+
+
+if __name__ == "__main__":
+    tenant_codes = ["jgg","bill"]
+
+    while True:
+        time.sleep(10)
+        uid = str(random.randint(1,10000)).rjust(6, "0")
+        tracker(rand(tenant_codes), uid)
---
+++ spark/src/main/scala/me/jingege/bigdata/spark/userlog/UserLogSpark.scala
@@ -0,0 +1,16 @@
+﻿﻿package me.jingege.bigdata.spark.userlog
+
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object UserLogSpark {
+
+  def main(args:Array[String]):Unit = {
+    val topic = "userlog"
+
+    val sparkConf = new SparkConf().setAppName("userlog").setMaster("local[1]")
+    val sc = new SparkContext(sparkConf)
+//    val ssc = new StreamingContext(sparkConf,10)
+  }
+
+}
